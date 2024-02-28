7464de64ddd1bf903c061926e95e29afbd1f9067
Sutthikarn <kimotorihana@gmail.com>
2024-02-28 20:14:09 +0700

    New Menu Function

--- components/main.py
+++ components/main.py
@@ -11,60 +11,69 @@ class MainWidget(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
-        self.canvas.clear()
+        self.construct_main_menu()
+        
+    def on_back_button_pressed(self, instance):
+        self.construct_main_menu()
+        
+    def construct_main_menu(self):
+
+        self.clear_layout()
+
         with self.canvas:
             Color(1, 1, 1, 0)  
             self.rect = Rectangle(size=self.size, pos=self.pos)
 
         self.background_image = Image(source="maxresdefault.jpg")  
         self.add_widget(self.background_image)
-
         self.background_image.allow_stretch = True
         self.background_image.keep_ratio = False
         self.background_image.pos = (0, 0)
         self.background_image.size = (Window.width, Window.height)
         
-        layout = BoxLayout(orientation='vertical', spacing=20)
+        self.title_label = Label(text="Bookworm Adventures", font_size=48, color="black")
+        self.title_label.size_hint = (None, None)
+        self.title_label.size = (self.width, 100)
+        self.title_label.center_x = Window.width / 2
+        self.title_label.top = Window.height - 50
+        self.add_widget(self.title_label)
+        
+        layout = BoxLayout(orientation='vertical', spacing=20, padding=(10, 10))
         layout.size_hint = (None, None)
         layout.width = self.width  
         layout.height = self.height  
-        layout.pos = (Window.width - self.width) / 2, (Window.height - self.height) / 2
+        layout.center = self.center
         self.add_widget(layout)
         
-        self.title_label = Label(text="Bookworm Adventures", font_size=48)
-        layout.add_widget(self.title_label)
-        
-        self.play_button = Button(text="Play")
-        self.play_button.bind(on_press=self.on_play_button_pressed)  
+        self.play_button = Button(text="Play", size_hint=(None, None), size=(200, 60))
+        self.play_button.bind(on_press=self.construct_game_menu)
         layout.add_widget(self.play_button)
         
-        self.options_button = Button(text="Options")
+        self.options_button = Button(text="Options", size_hint=(None, None), size=(200, 60))
         layout.add_widget(self.options_button)
 
-    def on_play_button_pressed(self, instance):
+    def clear_layout(self):
+        for widget in self.children[:]:
+            if isinstance(widget, BoxLayout):
+                self.remove_widget(widget)
 
-        self.clear_widgets()  
-        
-        
-        new_layout = BoxLayout(orientation='vertical', spacing=20)
+    def construct_game_menu(self, instance):
+
+        self.clear_layout()
+
+        new_layout = BoxLayout(orientation='vertical', spacing=20, padding=(10, 10))
         new_layout.size_hint = (None, None)
         new_layout.width = self.width  
         new_layout.height = self.height  
-        new_layout.pos = (Window.width - self.width) / 2, (Window.height - self.height) / 2
+        new_layout.center = self.center
         self.add_widget(new_layout)
         
-        new_title_label = Label(text="Game Started!", font_size=48)
+        new_title_label = Label(text="Game Started!", font_size=48, color="black")
         new_layout.add_widget(new_title_label)
         
         new_back_button = Button(text="Back to Main Menu")
-        new_back_button.bind(on_press=self.on_back_button_pressed)  
+        new_back_button.bind(on_press=self.on_back_button_pressed)
         new_layout.add_widget(new_back_button)
-
-    def on_back_button_pressed(self, instance):
-
-        self.clear_widgets()  
-        self.__init__()  
-
 
 class MainApp(App):
     def build(self):
